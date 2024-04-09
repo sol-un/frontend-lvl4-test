96172572d8c82634f08573566e5f79e41acff99d
sol-un <solovyev833@gmail.com>
2024-04-09 17:07:10 +0300

    change greeting from Hexlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi from Hexlet!
+          Hello from Hexlet!👋🎓
         </p>
         <a
           className="App-link"
